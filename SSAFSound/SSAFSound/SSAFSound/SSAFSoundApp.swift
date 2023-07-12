32aa3a36995f220907db9ef3c52c86547ec7637d
realhsb <realhsb@gmail.com>
2023-07-12 10:41:34 +0900

    ✨[feat]: 프로젝트 추가

---
+++ SSAFSound/SSAFSound/SSAFSound/SSAFSoundApp.swift
@@ -0,0 +1,17 @@
+//
+//  SSAFSoundApp.swift
+//  SSAFSound
+//
+//  Created by Subeen on 2023/07/12.
+//
+
+import SwiftUI
+
+@main
+struct SSAFSoundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
