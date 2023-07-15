01fb76d9f213e929d63c65ee25454d8f2ae86ff4
YimKeul <leesjun29@gmail.com>
2023-07-15 21:50:42 +0900

    ✨[feat]: 게시판 ui 추가
    게시판 ui 컴포넌트화

--- SSAFSound/SSAFSound/SSAFSound/Sources/UI/DesignSystem/View/BoardItem/BoardItemView.swift
+++ SSAFSound/SSAFSound/SSAFSound/Sources/UI/DesignSystem/View/BoardItem/BoardItemView.swift
@@ -7,14 +7,35 @@
 
 import SwiftUI
 
-struct BoardItem: View {
+struct BoardItemView: View {
+    
+    let BoardName: String
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack{
+            Text("\(BoardName)\n게시판")
+                .multilineTextAlignment(.leading)
+
+                .font(
+                    .manropeFont(family: .SemiBold, size: 15)
+                    .weight(.semibold)
+                )
+                .padding(.leading,25)
+                .foregroundColor(.ssafSoundColor(.black))
+                .frame(height: 44.10422)
+                
+                
+            Spacer()
+        }.frame(width: 340, height: 105)
+            .padding(.bottom, 13)
+            .background(.white)
+            .cornerRadius(20)
+            
     }
 }
 
-struct BoardItem_Previews: PreviewProvider {
+struct BoardItemView_Previews: PreviewProvider {
     static var previews: some View {
-        BoardItem()
+        BoardItemView(BoardName: "자유게시판")
     }
 }
