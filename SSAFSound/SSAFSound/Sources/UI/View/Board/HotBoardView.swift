fd0a2936c5c09ee57993e6147e6915e7203a4021
Yimkeul <leesjun29@gmail.com>
2023-07-23 22:02:07 +0900

    ✨[feat]: 게시판 검색 UI

--- SSAFSound/SSAFSound/Sources/UI/View/Board/HotBoardView.swift
+++ SSAFSound/SSAFSound/Sources/UI/View/Board/HotBoardView.swift
@@ -8,15 +8,18 @@
 import SwiftUI
 
 struct HotBoardView: View {
+    
     let boardName: String
+    @State var serachRecuritText: String = ""
+    
     var body: some View {
         ZStack {
             Color.background
                 .edgesIgnoringSafeArea(.all)
             NavigationStack{
                 VStack{
+                    searchRecuirTextFieldView()
                     ScrollView{
-                        
                         ForEach(0..<10) { i in
                             NavigationLink(destination:PostView()){HotBoardItemView().toolbarRole(.editor)}
                         }
@@ -24,6 +27,38 @@ struct HotBoardView: View {
                 }
             }
         }
+    }
+    
+    @ViewBuilder
+    private func searchRecuirTextFieldView() -> some View {
+        Spacer()
+            .frame(height: 8)
+        
+        VStack {
+            RoundedRectangle(cornerRadius: 20)
+                .fill(.white)
+                .frame(height: 42)
+                .overlay {
+                    HStack {
+                        TextField("검색어를 입력해주세요", text: $serachRecuritText)
+                            .manropeFont(family: .Medium, size: 18)
+                            .kerning(-0.3)
+                        
+                        Spacer()
+                        
+                        Image(systemName: "magnifyingglass")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 18, height: 15)
+                        
+                        
+                    }
+                    .padding(.horizontal, 20)
+                }
+                
+        }
+        .padding(.horizontal, 20)
+        .padding(.bottom, 20)
     }
 }
 
--- SSAFSound/SSAFSound/Sources/UI/View/Board/SelectedBoardView.swift
+++ SSAFSound/SSAFSound/Sources/UI/View/Board/SelectedBoardView.swift
@@ -9,6 +9,8 @@ import SwiftUI
 
 struct SelectedBoardView: View {
     let boardName: String
+    @State var serachRecuritText: String = ""
+    
     var body: some View {
         
         ZStack {
@@ -16,18 +18,51 @@ struct SelectedBoardView: View {
                 .edgesIgnoringSafeArea(.all)
             NavigationStack{
                 VStack{
-                ScrollView{
-                    ForEach(0..<10) { i in
-                        NavigationLink(destination: PostView() ){
-                            SelectedBoardItem().toolbarRole(.editor)
+                    searchRecuirTextFieldView()
+                    ScrollView{
+                        ForEach(0..<10) { i in
+                            NavigationLink(destination: PostView() ){
+                                SelectedBoardItem().toolbarRole(.editor)
+                            }
                         }
                     }
+                    .padding(.horizontal,25)
                 }
-                .padding(.horizontal,25)
-            }}
+            }
             FloatingButton()
         }
         
+    }
+    @ViewBuilder
+    private func searchRecuirTextFieldView() -> some View {
+        Spacer()
+            .frame(height: 8)
+        
+        VStack {
+            RoundedRectangle(cornerRadius: 20)
+                .fill(.white)
+                .frame(height: 42)
+                .overlay {
+                    HStack {
+                        TextField("검색어를 입력해주세요", text: $serachRecuritText)
+                            .manropeFont(family: .Medium, size: 18)
+                            .kerning(-0.3)
+                        
+                        Spacer()
+                        
+                        Image(systemName: "magnifyingglass")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 18, height: 15)
+                        
+                        
+                    }
+                    .padding(.horizontal, 20)
+                }
+                
+        }
+        .padding(.horizontal, 20)
+        .padding(.bottom, 20)
     }
 }
 
