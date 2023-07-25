490cee3e02401845653ca48813c53ef4ead78f59
realhsb <realhsb@gmail.com>
2023-07-25 20:37:23 +0900

    🪛[chore]: 캠퍼스 선택 뷰 코드 수정

--- SSAFSound/SSAFSound/SSAFSound/Sources/UI/View/SignUp/SignUpCampusView.swift
+++ SSAFSound/SSAFSound/SSAFSound/Sources/UI/View/SignUp/SignUpCampusView.swift
@@ -8,22 +8,68 @@
 import SwiftUI
 
 struct SignUpCampusView: View {
+//    @State var path: [StackViewType] = []
+    
     @State private var campus: CampusMenuOption? = nil
+    @State private var path = NavigationPath()
+    @State var isLinkActive = false
+    
+    private var title: String = """
+                                SSAFY\n캠퍼스를 선택해주세요
+                                """
     var body: some View {
-        ZStack {
-            Color.background.edgesIgnoringSafeArea(.all)
-            
-            VStack {
-                Text("")
-                
-                DropdownMenu(selectedOption: self.$campus, placeholder: "Select your campus", options: CampusMenuOption.allCampus)
+        NavigationStack(path: $path) {
+            ZStack {
+                Color.background.edgesIgnoringSafeArea(.all)
                     
+                    VStack {
+                        ProgressView("Loading...", value: 0.6, total: 1)
+                                .progressViewStyle(CustomProgressBar())
+                                
+                        Spacer()
+                            .frame(height: 60)
+                        
+                        // SSAFSOUND 한글 B/28
+                        Text(title)
+                          .font(
+                            Font.manropeFont(family: .Bold, size: 28)
+                          )
+                          .foregroundColor(.white)
+                          .frame(width: 344, height: 84, alignment: .topLeading)
+                            
+                        Spacer()
+                            .frame(height: 33)
+                        
+                        DropdownMenu(selectedOption: self.$campus, placeholder: "Select your campus", options: CampusMenuOption.allCampus)
+                            .onTapGesture {
+                                path.append("isMajor")
+                                isLinkActive = true
+                            }
+                        
+                        Spacer()
 
-                
+                    }
+            }
+            
+            .navigationDestination(isPresented: $isLinkActive) {
+                SignUpIsMajorView()
+            }
+            .toolbar {
+                ToolbarItemGroup(placement: .navigationBarLeading) {
+                    Button() {
+                        print("Back")
+                        
+                    } label: {
+                        Image(systemName: "chevron.left")
+                    }
+                }
             }
         }
-    }
-    
+            
+            
+        
+        
+        }
 }
 
 struct LoginSsafyView_Previews: PreviewProvider {
