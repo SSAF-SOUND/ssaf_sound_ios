26281521dc5aa7cc94ef01419d2f2c130f8a1cdd
realhsb <realhsb@gmail.com>
2023-08-09 22:08:02 +0900

    ✨[feat]: 싸피 인증 성공 유저의 네임 카드 추가
    닉네임이 2줄 이상 표시 안 되는 것 수정해야 함

---
+++ SSAFSound/SSAFSound/Sources/UI/View/SignUp/NameCard/NameCardView.swift
@@ -0,0 +1,66 @@
+//
+//  NameCardView.swift
+//  SSAFSound
+//
+//  Created by Subeen on 2023/08/09.
+//
+
+import SwiftUI
+
+struct NameCardView: View {
+    //MARK: - PROPERTIES
+    
+    var nickname: String
+    var campus: String
+    var semester: String
+    var isMajor: Bool
+    var trackImage: ImageAsset
+    
+    
+    
+    var body: some View {
+        ZStack {
+            RoundedRectangle(cornerRadius: 10)
+                .foregroundColor(.whilte)
+            
+            VStack(alignment: .leading, spacing: 0) {
+                HStack(spacing: 2) {
+                    Circle()
+                        .frame(width: 40, height: 40)
+                        .foregroundColor(.ssafSoundColor(isMajor ? .major : .nonMajor))
+                    
+                    // 줄바꿈 안 되는 현상 고칠 것
+                    Text(nickname)
+                        .multilineTextAlignment(.leading)
+                        .foregroundColor(.ssafySoundblack)
+                        .manropeFont(family: .Bold, size: 40)
+                        
+                        
+                    Image(asset: trackImage)
+                        .resizable()
+                        .frame(width: 32, height: 32)
+                }
+                
+                HStack() {
+                    Text(campus)
+                    Text("SSAFY")
+                    Text(semester)
+                }
+                .foregroundColor(.ssafySoundblack)
+                .fontWeight(.medium)
+            }
+            .frame(maxWidth: 320)
+            .padding(.horizontal, 10)
+            .padding(.vertical, 6)
+        } //: ZSTACK
+        .fixedSize()
+    }
+}
+
+struct NameCardView_Previews: PreviewProvider {
+    static var previews: some View {
+        NameCardView(nickname: "James", campus: "서울캠퍼스", semester: "10기", isMajor: false, trackImage: .track_fallback_primary)
+            .previewLayout(.sizeThatFits)
+            .preferredColorScheme(.dark)
+    }
+}
