b4949c11d8c1e0744489e3a5c377f706eb5ca20c
Roy-wonji <shuwj81@daum.net>
2023-10-12 21:18:21 +0900

    ✨[feat] : 리쿠르팅  파일  수정

---
+++ SSAFSound/SSAFSound/Sources/Domain/API/SSAFSoundAPI/SSAFSoundAPIRecruitInvite.swift
@@ -0,0 +1,16 @@
+//
+//  SSAFSoundAPIRecruitInvite.swift
+//  SSAFSound
+//
+//  Created by 서원지 on 10/12/23.
+//
+
+import Foundation
+
+public enum SSAFSoundAPIRecruitParticipant {
+    public static let recruitsParticipan = String("recruits/1/application")
+    public static let recruitsParticipanApprove = String("recruit-applications/1/approve")
+    public static let recruitsRejection = String("recruit-applications/1/reject")
+    public static let recruitsApplicant = String("recruit-applications/1/reject")
+   
+}
